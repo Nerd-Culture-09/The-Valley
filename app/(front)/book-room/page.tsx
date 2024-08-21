f70bfe1da2d3cadaa5e7e10d994e221f02332837
Mswati Tshabalala <mswatitshabalala03@gmail.com>
2024-08-22 00:47:47 +0200

    individual-room-page-done, book-room + pricing  initialized

---
+++ app/(front)/book-room/page.tsx
@@ -0,0 +1,16 @@
+
+
+import BookRoom from '@/components/FrontEnd/BookRoom';
+import React from 'react'
+
+function page() {
+  return (
+    <div className='md-bg-black'>
+     
+        <BookRoom />
+     
+    </div>
+  )
+}
+
+export default page;
--- components/ui/RoomAmenties.tsx
+++ components/ui/RoomAmenties.tsx
@@ -2,41 +2,33 @@ import { FaBath, FaTv, FaWifi } from 'react-icons/fa';
 
 const RoomAmenitiesCard = () => {
   return (
-    <div className="max-w-sm mx-auto bg-white rounded-lg shadow-md p-6">
+    <div className=" relative max-w-sm mx-auto bg-white rounded-lg shadow-md p-6">
+      
       <div className="flex items-center mb-4">
-        <div className="w-12 h-12 flex items-center justify-center">
-          <div className="text-black text-3xl font-medium font-['Inter']">2</div>
-        </div>
-        <div className="ml-4 text-black text-3xl font-bold font-['Inter']">
+     
+        <div className="ml-4 text-black text-3xl">
           Our Room Amenities
         </div>
       </div>
       <div className="flex items-center mb-4">
-        <FaBath className="w-12 h-12 text-blue-500" />
-        <div className="ml-4 text-black text-3xl font-medium font-['Inter']">
+        <FaBath className="w-8 h-8 text-blue-500" />
+        <div className="ml-4 text-black text-1xl ">
           Bathroom
         </div>
       </div>
       <div className="flex items-center mb-4">
-        <FaTv className="w-12 h-12 text-blue-500" />
-        <div className="ml-4 text-black text-3xl font-medium font-['Inter']">
+        <FaTv className="w-8 h-8  text-blue-500" />
+        <div className="ml-4 text-black text-1xl ">
           TV
         </div>
       </div>
       <div className="flex items-center mb-4">
-        <FaWifi className="w-12 h-12 text-blue-500" />
-        <div className="ml-4 text-black text-3xl font-medium font-['Inter']">
+        <FaWifi className="w-8 h-8  text-blue-500" />
+        <div className="ml-4 text-black text-1xl ">
           Free Wifi
         </div>
       </div>
-      <div className="mt-6">
-        <div className="w-full text-black text-[40px] font-bold font-['Inter']">
-          North Branch
-        </div>
-        <div className="w-full text-black text-[35px] font-semibold font-['Inter']">
-          Room 01
-        </div>
-      </div>
+     
     </div>
   );
 };
