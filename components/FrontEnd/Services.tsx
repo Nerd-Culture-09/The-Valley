1cb7eca2f5430b74346b54117b2e57e35bffdd38
Stledy <152036434+Stledy@users.noreply.github.com>
2024-08-22 01:18:50 +0200

    north-rooms

--- components/FrontEnd/Services.tsx
+++ components/FrontEnd/Services.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Services = () => {
   return (
@@ -9,29 +10,34 @@ const Services = () => {
 
       <div className="relative w-full max-w-screen-xl h-[821px] border-black/50 flex flex-col items-center gap-8 p-4">
         <div className="w-full max-w-screen-xl bg-[#f3f3f3]/40 rounded-[20px] flex justify-center items-center gap-20">
-          {/* Container for Room Image */}
-          <div className="w-[400px] h-[760px] rounded-[42px] overflow-hidden flex items-end relative">
-            <img 
-              src="/north.jpg" 
-              alt="North" 
-              className="w-full h-full object-cover"
-            />
-            <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-black text-2xl font-bold ">
-              North Branch
+          
+          {/* North Branch Container */}
+          <Link href="/NorthRoomPage">
+            <div className="w-[400px] h-[760px] rounded-[42px] overflow-hidden flex items-end relative cursor-pointer">
+              <img 
+                src="/north.jpg" 
+                alt="North" 
+                className="w-full h-full object-cover"
+              />
+              <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-black text-2xl font-bold ">
+                North Branch
+              </div>
             </div>
-          </div>
+          </Link>
 
-          {/* Container for Breakfast Image */}
-          <div className="w-[400px] h-[760px] rounded-[42px] overflow-hidden flex items-end relative">
-            <img 
-              src="./south.jpg" 
-              alt="South" 
-              className="w-full h-full object-cover"
-            />
-            <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-black text-2xl font-bold">
-              South Branch
+          {/* South Branch Container */}
+          <Link href="/south">
+            <div className="w-[400px] h-[760px] rounded-[42px] overflow-hidden flex items-end relative cursor-pointer">
+              <img 
+                src="./south.jpg" 
+                alt="South" 
+                className="w-full h-full object-cover"
+              />
+              <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-black text-2xl font-bold">
+                South Branch
+              </div>
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
