6364b29b59ce7e69cbbeb47f14436c6c8f392dc5
Mswati Tshabalala <mswatitshabalala03@gmail.com>
2024-08-25 23:00:44 +0200

    rooms-center-on-md,book-component-compatibility

--- components/FrontEnd/AvailableRooms.tsx
+++ components/FrontEnd/AvailableRooms.tsx
@@ -1,9 +1,18 @@
 "use client";
 import { useState, ReactNode } from "react";
-import { FaTable, FaBed, FaShieldAlt, FaCar, FaWifi, FaBath, FaFire, FaTimes } from 'react-icons/fa';
+import {
+  FaTable,
+  FaBed,
+  FaShieldAlt,
+  FaCar,
+  FaWifi,
+  FaBath,
+  FaFire,
+  FaTimes,
+} from "react-icons/fa";
 import { DirectionAwareHover } from "../ui/direction-aware-hover";
 import { RoomCarouselDemo } from "./RoomCarousel";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { SelectDemo } from "./RoomPackeges";
 
 interface CardData {
@@ -17,73 +26,108 @@ interface CardData {
 // Image arrays for each room
 const room1Imgs = [
   "/valley_south_room1_bedroom.jpeg",
-  "/valley_south_room1_livingroom.jpeg",
+  "/bedroom_front_display.jpg",
   "/valley_south_room1_bathroom.jpeg",
-  "/valley_south_room1_kitchen.jpeg",
-  "/valley_south_room1_balcony.jpeg"
+  "/valley_south_outside_night.jpeg",
+  "/valley_south_town_view.jpeg",
 ];
 
 const room2Imgs = [
   "/valley_south_room2_bedroom.jpeg",
-  "/valley_south_room2_livingroom.jpeg",
-  "/valley_south_room2_bathroom.jpeg",
-  "/valley_south_room2_kitchen.jpeg",
-  "/valley_south_room2_balcony.jpeg"
+  "/valley_south_room2_shower.jpeg",
+  "/bedroom_front_display3.jpg",
+  "/valley_south_outside_night.jpeg",
+  "/valley_south_town_view.jpeg",
 ];
 
 const room3Imgs = [
-  "/bedroom_display1.jpeg",
-  "/livingroom_display1.jpeg",
-  "/bathroom_display1.jpeg",
-  "/kitchen_display1.jpeg",
-  "/balcony_display1.jpeg"
+  "/valley_south_room3_bedroom.jpeg",
+  "/valley_south_room3_bathroom.jpeg",
+  "/bedroom_front_display3.jpg",
+  "/valley_south_outside_night.jpeg",
+  "/valley_south_town_view.jpeg",
 ];
 
 const room4Imgs = [
   "/bedroom_display2.jpeg",
-  "/livingroom_display2.jpeg",
-  "/bathroom_display2.jpeg",
-  "/kitchen_display2.jpeg",
-  "/balcony_display2.jpeg"
+  "/valley_south_room2_restroom.jpeg",
+  "/valley_south_room2_shower.jpeg",
+  "/valley_south_outside_night.jpeg",
+  "/valley_south_town_view.jpeg",
 ];
 
 // Array of amenities with icons
 const amenities = [
-  { label: "Table", icon: <FaTable className="text-[#ff7665] text-3xl mt-4" /> },
+  {
+    label: "Table",
+    icon: <FaTable className="text-[#ff7665] text-3xl mt-4" />,
+  },
   { label: "Double", icon: <FaBed className="text-[#ff7665] text-3xl mt-4" /> },
-  { label: "Security", icon: <FaShieldAlt className="text-[#ff7665] text-3xl mt-4" /> },
-  { label: "Parking", icon: <FaCar className="text-[#ff7665] text-3xl mt-4" /> },
-  { label: "Free Wifi", icon: <FaWifi className="text-[#ff7665] text-3xl mt-4" /> },
-  { label: "Bathroom", icon: <FaBath className="text-[#ff7665] text-3xl mt-4" /> },
-  { label: "Heater", icon: <FaFire className="text-[#ff7665] text-3xl mt-4" /> },
-  { label: "Blanket", icon: <FaFire className="text-[#ff7665] text-3xl mt-4" /> }
+  {
+    label: "Security",
+    icon: <FaShieldAlt className="text-[#ff7665] text-3xl mt-4" />,
+  },
+  {
+    label: "Parking",
+    icon: <FaCar className="text-[#ff7665] text-3xl mt-4" />,
+  },
+  {
+    label: "Free Wifi",
+    icon: <FaWifi className="text-[#ff7665] text-3xl mt-4" />,
+  },
+  {
+    label: "Bathroom",
+    icon: <FaBath className="text-[#ff7665] text-3xl mt-4" />,
+  },
+  {
+    label: "Heater",
+    icon: <FaFire className="text-[#ff7665] text-3xl mt-4" />,
+  },
+  {
+    label: "Blanket",
+    icon: <FaFire className="text-[#ff7665] text-3xl mt-4" />,
+  },
 ];
 
 // DummyContent component to display the popup content
-const DummyContent = ({ category, title, images }: { category: string; title: string; images: string[] }) => {
+const DummyContent = ({
+  category,
+  title,
+  images,
+}: {
+  category: string;
+  title: string;
+  images: string[];
+}) => {
   return (
     <>
       <div className="flex flex-col lg:flex-row lg:space-x-8">
         <div className="lg:w-1/2">
-        <div className="ml-5">
-          <SelectDemo/>
-        </div>
-        <div  className="-mt-28">
-
-          <RoomCarouselDemo images={images} />
-          <div className="w-full flex justify-center p-5">
-            <Button>Book now</Button>
+          <div className="ml-5">
+            <SelectDemo />
           </div>
-        </div>
+          <div className="-mt-28">
+            <RoomCarouselDemo images={images} />
+            <div className="w-full flex justify-center p-5">
+              <Button>Book now</Button>
+            </div>
+          </div>
         </div>
         <div className="lg:w-1/2 mt-8 lg:mt-0">
           <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl">
-            <h3 className="text-neutral-700 dark:text-neutral-200 text-xl font-bold mb-2">{category}</h3>
-            <h4 className="text-neutral-700 dark:text-neutral-200 text-lg mb-4">{title}</h4>  
+            <h3 className="text-neutral-700 dark:text-neutral-200 text-xl font-bold mb-2">
+              {category}
+            </h3>
+            <h4 className="text-neutral-700 dark:text-neutral-200 text-lg mb-4">
+              {title}
+            </h4>
             {/* Individual Amenities with Icons */}
             <div className="flex flex-wrap justify-between mt-10">
               {amenities.map((amenity, index) => (
-                <div key={index} className="w-[96px] h-[96px] flex flex-col items-center bg-white rounded-lg border border-[#e8e8e8] p-4">
+                <div
+                  key={index}
+                  className="w-[96px] h-[96px] flex flex-col items-center bg-white rounded-lg border border-[#e8e8e8] p-4"
+                >
                   {amenity.icon}
                   <div className="mt-2 text-xs">{amenity.label}</div>
                 </div>
@@ -101,30 +145,30 @@ const cardsData: CardData[] = [
   {
     category: "The Valley South",
     title: "Room 1",
-    src: "/valley_south_room1_bedroom.jpeg",
+    src: "/bedroom_front_display3.jpg",
     images: room1Imgs,
-    price: "R250/ Night"
+    price: "R250/ Night",
   },
   {
     category: "The Valley South",
     title: "Room 2",
-    src: "/valley_south_room2_bedroom.jpeg",
+    src: "/bedroom_front_display.jpg",
     images: room2Imgs,
-    price: "R250/ Night"
+    price: "R250/ Night",
   },
   {
     category: "The Valley North",
     title: "Room 1",
     src: "/bedroom_display1.jpeg",
     images: room3Imgs,
-    price: "R250/ Night"
+    price: "R250/ Night",
   },
   {
     category: "The Valley North",
     title: "Room 2",
     src: "/bedroom_display2.jpeg",
     images: room4Imgs,
-    price: "R250/ Night"
+    price: "R250/ Night",
   },
 ];
 
@@ -133,7 +177,9 @@ export function CardDemo() {
   const [popupContent, setPopupContent] = useState<ReactNode | null>(null);
 
   const handleClick = (category: string, title: string, images: string[]) => {
-    setPopupContent(<DummyContent category={category} title={title} images={images} />);
+    setPopupContent(
+      <DummyContent category={category} title={title} images={images} />
+    );
     setShowPopup(true);
   };
 
@@ -145,12 +191,15 @@ export function CardDemo() {
     <div className="p-4">
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-xl font-bold">Available Rooms</h2>
-        <a href="#" className="text-sm font-semibold text-indigo-600 hover:underline">
+        <a
+          href="#"
+          className="text-sm font-semibold text-indigo-600 hover:underline"
+        >
           View All
         </a>
       </div>
 
-      <div className="flex flex-wrap justify-between gap-2">
+      <div className="flex flex-wrap md:flex-row flex-col md:justify-between justify-center items-center gap-2">
         {cardsData.map((card, index) => (
           <div
             key={index}
@@ -158,7 +207,9 @@ export function CardDemo() {
             className="cursor-pointer"
           >
             <DirectionAwareHover imageUrl={card.src}>
-              <p className="font-bold text-xl">{card.title}</p>
+              <p className="font-bold text-xl">
+                {card.category + " " + card.title}
+              </p>
               <p className="font-normal text-sm">{card.price}</p>
             </DirectionAwareHover>
           </div>
@@ -170,7 +221,7 @@ export function CardDemo() {
           <div className="bg-white p-8 w-full max-w-4xl relative border-2 border-gray-200 rounded-lg md:rounded-2xl">
             <button
               onClick={handleClose}
-              className="absolute top-3.5 right-1 text-gray-500 hover:text-gray-700"
+              className="absolute top-2 right-1 text-gray-500 hover:text-gray-700"
               aria-label="Close"
             >
               <FaTimes className="text-2xl" />
--- components/FrontEnd/BackGroundBox-bar.tsx
+++ components/FrontEnd/BackGroundBox-bar.tsx
@@ -8,13 +8,13 @@ import { InputWithButton } from "./numberOfPeople"; // Import the InputWithButto
 export function BackGroundBoxBar() {
   return (
     <div
-      className="relative w-full overflow-hidden bg-slate-900 flex flex-row justify-center items-center"
-      style={{ height: "2.5cm" }} // Adjusted height to 2.5cm
+      className="relative w-full overflow-hidden bg-slate-900 flex flex-col md:flex-row justify-center items-center"
+      style={{ height: "auto", minHeight: "2.5cm" }} // Adjusted height for flexibility
     >
       <div className="absolute inset-0 w-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
       {/* Flex container to align all elements */}
-      <div className="relative z-30 flex flex-row justify-start items-center gap-x-4 px-4">
+      <div className="relative z-30 flex flex-col md:flex-row justify-start items-center gap-y-4 md:gap-y-0 gap-x-4 px-4 py-2 md:py-0">
         {/* Date Picker and Time Picker Section */}
         <div className="flex flex-col items-center">
           <span className="text-white text-sm mb-1">Check In</span>
