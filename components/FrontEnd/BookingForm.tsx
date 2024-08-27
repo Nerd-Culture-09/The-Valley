91f0d96aec6db441dce2e769bedfc5429a6310c1
Mswati Tshabalala <mswatitshabalala03@gmail.com>
2024-08-27 12:31:12 +0200

    booking-form-adjustments,pricing-adjustments

--- components/FrontEnd/BookingForm.tsx
+++ components/FrontEnd/BookingForm.tsx
@@ -1,6 +1,16 @@
 "use client";
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "../ui/checkbox";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectLabel,
+  SelectItem,
+} from "@radix-ui/react-select";
 
 interface FormPageProps {
   onNext: () => void;
@@ -14,7 +24,6 @@ export const FormPage: React.FC<FormPageProps> = ({ onNext }) => {
 
   return (
     <div className="p-4">
-    
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-700">
@@ -35,6 +44,54 @@ export const FormPage: React.FC<FormPageProps> = ({ onNext }) => {
             required
             className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
           />
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700">
+            Phone Number
+          </label>
+          <input
+            type="text"
+            required
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700">
+            Booking for :
+          </label>
+          <div className="flex items-center space-x-2  text-gray-700">
+            <Checkbox id="terms" />
+            <label
+              htmlFor="terms"
+              className="text-sm font-medium leading-none  text-gray-700 "
+            >
+              Myself
+            </label>
+          </div>
+          <div className="flex items-center mt-1 space-x-2 text-gray-700">
+            <Checkbox id="terms" />
+            <label
+              htmlFor="terms"
+              className="text-sm font-medium leading-none  text-gray-700"
+            >
+              Someone Else
+            </label>
+          </div>
+          <label
+            
+            className="block mt-2 text-sm font-medium  text-gray-700"
+          >
+            Select Payment Method
+          </label>
+          <select
+            id="countries"
+            className="bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option selected>Select Payment Method</option>
+            <option value="ecocash">Ecocash - Merchant Code : 64382 Names : Valley Maseru</option>
+            <option value="mpesa">Mpesa </option>
+           
+          </select>
         </div>
         <Button type="submit">Submit</Button>
       </form>
--- components/FrontEnd/Pricing.tsx
+++ components/FrontEnd/Pricing.tsx
@@ -1,110 +1,124 @@
 import Link from "next/link";
+import { Button } from "../ui/button";
 
 const Pricing = () => {
+  const plans = [
+    {
+      name: "Basic 2 Hour Package",
+      price: 100,
+      features: [
+        "2 People",
+        "Free Wifi",
+        "Microwaave",
+        "Single Bed",
+        "Electric Blanket",
+        "TV - easy access channels",
+        "Bathroom",
+        "Kettle",
+      ],
+      url: "#",
+    },
+    {
+      name: "Standard 2 Hour Package",
+      price: 150,
+      features: [
+        "2 People",
+        "Free Wifi",
+        "Microwave",
+        "Double Bed",
+        "Electric Blanket",
+        "TV - premium channels",
+        "Bathroom",
+        "Kettle",
+      ],
+      url: "#",
+    },
+    {
+      name: "Basic  All Night Package",
+      price: 250,
+      features: [
+        "2 People",
+        "Free Wifi",
+        "Microwave",
+        "Double Bed",
+        "Electric Blanket",
+        "TV - easy access channels",
+        "Bathroom",
+        "Kettle",
+      ],
+      url: "#",
+    },
+    {
+      name: "Standard All Night Package",
+      price: 350,
+      features: [
+        "2 People",
+        "Free Wifi",
+        "Microwave",
+        "Double Bed",
+        "Electric Blanket",
+        "TV- premium channels",
+        "Bathroom",
+        "Kettle",
+      ],
+      url: "#",
+    },
+  ];
 
-    const plans = [
-        {
-            name: "Basic plan",
-            price: 12,
-            features: [
-                "Curabitur faucibus",
-                "massa ut pretium maximus",
-                "Sed posuere nisi",
-                "Pellentesque eu nibh et neque",
-                "Suspendisse a leo",
-                "Praesent quis venenatis ipsum",
-                "Duis non diam vel tortor",
-
-            ],
-            url :"#"
-        },
-        {
-            name: "Startup",
-            price: 35,
-            features: [
-                "Curabitur faucibus",
-                "massa ut pretium maximus",
-                "Sed posuere nisi",
-                "Pellentesque eu nibh et neque",
-                "Suspendisse a leo",
-                "Praesent quis venenatis ipsum",
-                "Duis non diam vel tortor",
-            ],
-            url:"#"
-        },
-        {
-            name: "Enterprise",
-            price: 60,
-            features: [
-                "Curabitur faucibus",
-                "massa ut pretium maximus",
-                "Sed posuere nisi",
-                "Pellentesque eu nibh et neque",
-                "Suspendisse a leo",
-                "Praesent quis venenatis ipsum",
-                "Duis non diam vel tortor",
-            ],
-            url:"#"
-        },
-    ];
-
-    return (
-        <section className='py-14'>
-            <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
-                <div className='relative max-w-xl mx-auto sm:text-center'>
-                    <h3 className='text-gray-800 text-3xl font-semibold sm:text-4xl'>
-                        Pricing for all sizes
-                    </h3>
-                    <div className='mt-3 max-w-xl'>
-                        <p>
-                           View our wide range of packages and choose the one that suits you
-                        </p>
-                    </div>
+  return (
+    <section className="py-5 mt-40">
+      <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+        <div className="relative max-w-xl mx-auto sm:text-center">
+          <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+            Our Affordable Room Packages
+          </h3>
+          <div className="mt-3 max-w-xl">
+            <p>View our packages and choose the one that suits you</p>
+          </div>
+        </div>
+        <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {plans.map((item, idx) => (
+            <div
+              key={idx}
+              className="relative flex flex-col p-8 rounded-xl border-2"
+            >
+              <div>
+                <span className="text-indigo-600 font-medium">{item.name}</span>
+                <div className="mt-4 text-gray-800 text-3xl font-semibold">
+                  M{item.price}{" "}
+                  <span className="text-xl text-gray-600 font-normal">/mo</span>
                 </div>
-                <div className='mt-16 space-y-6 justify-center gap-6 sm:grid sm:grid-cols-2 sm:space-y-0 lg:grid-cols-3'>
-                    {
-                        plans.map((item, idx) => (
-                            <div key={idx} className='relative flex-1 flex items-stretch flex-col p-8 rounded-xl border-2'>
-                                <div>
-                                    <span className='text-indigo-600 font-medium'>
-                                        {item.name}
-                                    </span>
-                                    <div className='mt-4 text-gray-800 text-3xl font-semibold'>
-                                        ${item.price} <span className="text-xl text-gray-600 font-normal">/mo</span>
-                                    </div>
-                                </div>
-                                <ul className='py-8 space-y-3'>
-                                    {
-                                        item.features.map((featureItem, idx) => (
-                                            <li key={idx} className='flex items-center gap-5'>
-                                                <svg
-                                                    xmlns='http://www.w3.org/2000/svg'
-                                                    className='h-5 w-5 text-indigo-600'
-                                                    viewBox='0 0 20 20'
-                                                    fill='currentColor'>
-                                                    <path
-                                                        fill-rule='evenodd'
-                                                        d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
-                                                        clip-rule='evenodd'></path>
-                                                </svg>
-                                                {featureItem}
-                                            </li>
-                                        ))
-                                    }
-                                </ul>
-                                <div className="flex-1 flex items-end">
-                                    <button className='px-3 py-3 rounded-lg w-full font-semibold text-sm duration-150 text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700' >
-                                       <Link href={item.url}>
-                                        Get Started
-                                        </Link>
-                                    </button>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div>
+              </div>
+              <ul className="py-8 space-y-3">
+                {item.features.map((featureItem, idx) => (
+                  <li key={idx} className="flex items-center gap-5">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5 text-indigo-600"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                    {featureItem}
+                  </li>
+                ))}
+              </ul>
+              <div className="flex-1 flex items-end">
+                <Button className="px-3 py-3 rounded-lg w-full font-semibold text-sm duration-150 text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700">
+                  <Link href={item.url}>Start Booking</Link>
+                </Button>
+              </div>
             </div>
-        </section>
-    );
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
+
 export default Pricing;
