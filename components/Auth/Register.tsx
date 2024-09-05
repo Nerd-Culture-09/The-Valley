5844f740ce3a9cc6e8ff2038bf0cecf3902cd433
Fuma <leonardfuma26@gmail.com>
2024-09-05 13:34:22 +0200

    =

--- components/Auth/Register.tsx
+++ components/Auth/Register.tsx
@@ -49,7 +49,7 @@ export default function RegisterFormWitBg({
   }
   const [passwordInputType, PasswordToggleIcon] = usePasswordToggle();
   return (
-    <div className="w-full flex items-center justify-center h-screen lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
